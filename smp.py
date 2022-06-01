ac3005ed982bb4918f33cba3bcf530c80a534601
Vivekdt007 <83772260+Vivekdt007@users.noreply.github.com>
2022-06-01 20:56:42 +0530

    Update smp.py

--- smp.py
+++ smp.py
@@ -1,7 +1,8 @@
+import yfinance as yfs
 import streamlit as st
 
 from datetime import date
-import yfinance as yfs
+
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
